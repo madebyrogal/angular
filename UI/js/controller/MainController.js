ee6ff0dc570597d4a074dd7b30f744ec33978c4e
Tomasz Rogalski <tomasz@madebyrogal.com>
2017-02-09 14:29:48 +0100

    ADD getServicePrice

--- UI/js/controller/MainController.js
+++ UI/js/controller/MainController.js
@@ -1,7 +1,65 @@
 app.controller('MainController', ['$scope', function ($scope) {
         $scope.title = 'Helo word';
+        $scope.cartOffer = {
+            offerId: 123456,
+            additionalServices: [
+                {
+                    name: "Montaż",
+                    id: "MONTAZ"
+                },
+                {
+                    name: "Wniesienie",
+                    id: "WNIESIENIE"
+                }
+            ],
+            applicableAdditionalServices: [
+                {
+                    definitionId: "MONTAZ",
+                    validationResult: {
+                        configurations: [
+                            {
+                                available: true,
+                                price: {
+                                    amount: "45.12",
+                                    currency: "PLN"
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    definitionId: "WNIESIENIE",
+                    validationResult: {
+                        configurations: [
+                            {
+                                available: true,
+                                price: {
+                                    amount: "25.12",
+                                    currency: "PLN"
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        };
+
+        $scope.selectService = function (service, offerId, status) {
+            console.log('Change - service', service);
+            console.log('Change - offerId', offerId);
+            console.log('Change - status', status);
+        };
         
-        $scope.change = function(status) {
-            console.log('Change', status);
-        }
+        $scope.getServicePrice = function(applicableAdditionalServices, currentService) {
+            var price = '';
+            _.each(applicableAdditionalServices, function(applicableService) {
+                if(applicableService.definitionId === currentService.id ) {
+                    
+                    price = _.first(applicableService.validationResult.configurations).price.amount;
+                    price += ' ' + _.first(applicableService.validationResult.configurations).price.currency;
+                }
+            });
+            
+            return price;
+        };
     }]);

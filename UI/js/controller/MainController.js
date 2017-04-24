d63dcb8ff6c6dd7e95fe7ea7907a3be00cc20d36
Joanna Rogalska <joanna.rogalska@allegrogroup.com>
2017-04-24 21:30:04 +0200

    first simple aplication in angular

--- UI/js/controller/MainController.js
+++ UI/js/controller/MainController.js
@@ -1,4 +1,48 @@
+var model = {
+    user: "Adam"
+};
+
+app.run(function ($http){
+    $http.get('data.json').success(function (data) {
+        model.items = data;
+    });
+});
+
+app.filter('checkedItems', function (){
+    return function(items, showComplete){
+        var resultArr = [];
+        angular.forEach(items, function (item) {
+            if (item.done === false || showComplete === true) {
+                resultArr.push(item);
+            }
+        });
+        return resultArr;
+    };
+});
+
 app.controller('MainController', ['$scope', function ($scope) {
-        $scope.title = 'Helo word';
-        
+    console.log('model',model);
+        $scope.todo = model;
+
+        $scope.incompleteCount = function(){
+            var count = 0;
+            angular.forEach($scope.todo.items, function(item){
+                if(!item.done){
+                    count ++;
+                }
+            });
+            return count;
+
+        };
+
+        $scope.warningLevel = function(){
+            return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+        };
+
+        $scope.addNewItem = function(actionText){
+            $scope.todo.items.push({
+                action: actionText,
+                done: false
+            });
+        };
     }]);
